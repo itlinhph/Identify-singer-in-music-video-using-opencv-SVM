0f99037d7d52db4518801e7e8635863d8b7646e1
Linh Phan <linhphanhust@gmail.com>
2017-12-08 23:21:44 +0700

    edit writeToVector, train()

--- prepr.py
+++ prepr.py
@@ -3,72 +3,98 @@ import os, sys
 import cv2
 from PIL import Image
 from skimage import io
+from sklearn import svm
 
 rawPath = 'rawdata/'
 outputPath = 'output/'
 
 # dirs = ['1/']
-dirs = ['1/','2/','3/','4/','5/','6/','7/','8/']
 imgDataFinal =[]
-label = []
 
-def faceDetectAndResizeImg():
+def faceDetectAndResizeImg(inputDir):
     faceDetect = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-    for dir in dirs:
-        path = rawPath + dir
+
+    print('STARTING DETECT AND RESIZE FACE...\n')
+    listDirs = os.listdir(inputDir)
+    for subDir in listDirs:
+        path = inputDir + subDir + '/'
         for imgfile in os.listdir(path):
             if os.path.isfile(path+ imgfile):
                 img = cv2.imread(path + imgfile)
-                name, ext = os.path.splitext(imgfile)
                 # Detect Faces
                 faces = faceDetect.detectMultiScale(img)
-                # print(path+name+ext)
+                print(path + imgfile)
                 if (isinstance(faces, tuple) ):
-                    # print('Cannot Detect!')
+                    print('FALSE: ' + path + imgfile)
                     continue
                 x,y,w,h = faces[0][:4]
                 faceCrop = img[y:y + h, x:x + w]
                 croppedImg = cv2.resize(faceCrop, (32,32))
-                cv2.imwrite(outputPath + dir + name + ext, croppedImg)
+                if not os.path.exists(outputPath+subDir):
+                    os.makedirs(outputPath + subDir)
+                cv2.imwrite(outputPath + subDir +'/'+ imgfile , croppedImg)
 
     print('Face recognize Complete!')
 
 
-def faceToVetor():
-    imgData =[]
-    for dir in dirs:
-        path = outputPath + dir
+def faceToVetor(inputDir):
+    listImgFace =[]
+    listLabel = []
+    listDirs = os.listdir(inputDir)
+    for subDir in listDirs:
+        path = inputDir + subDir + '/'
         for imgfile in os.listdir(path):
             if os.path.isfile(path + imgfile):
                 img = io.imread(path + imgfile, as_grey=True)
-                name, ext = os.path.splitext(imgfile)
                 if(img.shape != (32, 32)):
-                    print('Remove: '+ path+name+ext)
+                    print('Remove: '+ path+imgfile)
                     continue
-                imgData.append(img)
-                Y_label = dirs.index(dir)
-                label.append(Y_label)
+                listImgFace.append(img)
+                Y_label = listDirs.index(subDir)
+                listLabel.append(Y_label)
                 
-    imgDataFinal = np.array(imgData)
-    Y_train = np.array(label)
-    # print(imgDataFinal.shape)
+    imgDataFinal = np.array(listImgFace)
+    Y_train = np.array(listLabel)
     X_train = imgDataFinal.reshape(len(imgDataFinal), 32*32)
-    X_train = np.round(X_train,4)
+    # X_train = np.round(X_train,5)
     print(X_train.shape)
     print(Y_train.shape)
+
+    return X_train, Y_train
+    
+def writeVector():
+    X_train, Y_train = faceToVetor(outputPath)
     fileWrite = open('vector.txt', 'w')
-    for i in range(0, len(imgDataFinal)):
+    for i in range(0, len(Y_train)):
         fileWrite.write(str(Y_train[i]) + ' ')
-        for i2 in range(0,1024):
-            fileWrite.write(str(i2)+':'+str(X_train[i][i2])+' ')
+        for i2 in range(0, 1024):
+            fileWrite.write(str(i2) + ':' + str(X_train[i][i2]) + ' ')
 
         fileWrite.write('\n')
     fileWrite.close()
     print('Save vector complete!')
 
 
-# faceDetectAndResizeImg()
-faceToVetor()
+def train():
+    X_train, Y_train = faceToVetor(outputPath)
+    X_test, Y_test = faceToVetor('test/')
+    clf = svm.SVC()
+    print(clf.fit(X_train, Y_train))
+    
+    print('train complete!')
+    Y_pre = clf.predict(X_test)
+    print(clf.support_vectors_)
+
+    print('Y Test:')
+    print(Y_test)
+    print('Y pre:')
+    print(Y_pre)
+    
+
+# faceDetectAndResizeImg(rawPath)
+# faceToVetor(outputPath)
+# writeVector()
+train()
 
 
 
